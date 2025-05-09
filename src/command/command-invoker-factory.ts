adeffce93e39ac73bc6c64322d71308c48c2cb40
Tuan Nguyen Van Quoc <winqti@gmail.com>
2025-05-09 14:27:18 +0700

    feat: implement hero command

--- src/command/command-invoker-factory.ts
+++ src/command/command-invoker-factory.ts
@@ -1,22 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { CommandInvoker } from "./command-invoker";
-import { Command, CommandHandler, ICommand } from "./command.interface";
-import { ModuleRef } from "@nestjs/core";
-import { getCommandHandlerMetadata } from "src/decorator/command-handler.decorator";
-import { WarriorDragonKillerHandler } from "./handler/warrior-dragon-killer/warrior-dragon-killer.handler";
+import { Injectable } from '@nestjs/common';
+import { CommandInvoker } from './command-invoker';
+import { Command, CommandHandler } from './command.interface';
+import { ModuleRef } from '@nestjs/core';
+import { getCommandHandlerMetadata } from 'src/decorator/command-handler.decorator';
+import { WarriorDragonKillerHandler } from './handler/warrior-dragon-killer/warrior-dragon-killer.handler';
 
 @Injectable()
 export class CommandInvokerFactory {
-    constructor(
-        private readonly moduleRef: ModuleRef
-    ) {
-        console.log("🚀 ~ CommandInvokerFactory<T> ~ moduleRef.modules:", moduleRef.container.getModules())
-    }
+  constructor(private readonly moduleRef: ModuleRef) {
+    console.log(
+      '🚀 ~ CommandInvokerFactory<T> ~ moduleRef.modules:',
+      moduleRef.container.getModules(),
+    );
+  }
 
-    createInvoker<T extends Command<unknown>>(token: symbol): CommandInvoker<T> {
-        const command =  this.moduleRef.get(token, {strict: false}) as CommandHandler<T>;
-        getCommandHandlerMetadata(WarriorDragonKillerHandler);
-        console.log("🚀 ~ CommandInvokerFactory<T ~ createInvoker ~ command:", command)
-        return new CommandInvoker(command);
-    }
+  createInvoker<T extends Command<unknown>>(token: symbol): CommandInvoker<T> {
+    const command = this.moduleRef.get(token, {
+      strict: false,
+    }) as CommandHandler<T>;
+    getCommandHandlerMetadata(WarriorDragonKillerHandler);
+    console.log(
+      '🚀 ~ CommandInvokerFactory<T ~ createInvoker ~ command:',
+      command,
+    );
+    return new CommandInvoker(command);
+  }
 }
